bdad4b9e401f6047077ef3ffc47ba97f632cfd8c
root <root@server1.pascalsuissa.com>
2022-12-06 10:31:55 -0500

    changes to ipsearch 12-06-22

--- ipsearch.sh
+++ ipsearch.sh
@@ -4,37 +4,97 @@ echo "Scan for an IP within system logs!"
 sleep 5s
 read ip
 
-
+sleep 2s
 echo "Apache Error Log Results:"
 grep $ip /usr/local/apache/logs/error_log
+sleep 2s
 
+echo ""
+echo ""
+echo ""
+
+sleep 2s
 echo "ModSec Log Results:"
 grep $ip /usr/local/apache/logs/modsec_audit.log
+sleep 2s
 
+echo ""
+echo ""
+echo ""
+
+sleep 2s
 echo "cPanel Access Log Results:"
 grep $ip /usr/local/cpanel/logs/access_log
+sleep 2s
 
+echo ""
+echo ""
+echo ""
+
+sleep 2s
 echo "cPanel Error Log Results:"
 grep $ip /usr/local/cpanel/logs/error_log
+sleept 2s
 
+echo ""
+echo ""
+echo ""
+
+sleep 2s
 echo "cPanel Stats Log Results:"
 grep $ip /usr/local/cpanel/logs/stats_log
+sleep 2s
 
+echo ""
+echo ""
+echo ""
+
+sleep 2s
 echo "cPanel Check Service Log Results:"
 grep $ip /var/log/chkservd.log
+sleep 2s
 
+echo ""
+echo ""
+echo ""
+
+sleep 2s
 echo "Exim Main Log Results:"
 grep $ip /var/log/exim_mainlog
+sleep 2s
 
+echo ""
+echo ""
+echo ""
+
+sleep 5s
 echo "Exim Panic Log Results:"
 grep $ip /var/log/exim_paniclog
+sleep 5s
 
+echo ""
+echo ""
+echo ""
+
+sleep 5s
 echo "Exim Reject Log Results:"
 grep $ip /var/log/exim_rejectlog
+sleep 5s
 
+echo ""
+echo ""
+echo ""
+
+sleep 5s
 echo "lfd Log Results:"
 grep $ip /var/log/lfd.log
+sleep 5s
 
+echo ""
+echo ""
+echo ""
+
+sleep 5s
 echo "Thank you for using the IP search script!"
 sleep 5s
 
