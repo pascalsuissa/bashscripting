8457639cbc1800023890d4173c8cd490117dc933
root <root@server1.pascalsuissa.com>
2022-12-10 21:08:51 -0500

    Solid update

--- ipsearch.sh
+++ ipsearch.sh
@@ -11,11 +11,11 @@ sleep 3s
 
 echo -e "\n-------------------\nApache Error Log Results:\n-------------------"
 grep_apache=$( grep -a $ip /usr/local/apache/logs/error_log )
-if [ -z e"$grep_apache" ]
+if [ -z "$grep_apache" ]
 then
-	echo -e "\nNo Results Found\n"
+	echo -e "\n***No Results Found***\n"
 else
-	echo -e "\nResults Found\n-------------\n"
+	echo -e "\n***Results Found***\n"
 	sleep 2s
 	echo $grep_apache
 	echo ""
@@ -26,122 +26,120 @@ echo -e "--------------------\nModSec Log Results:\n--------------------"
 grep_modsec=$( grep -a $ip /usr/local/apache/logs/modsec_audit.log )
 if [ -z "$grep_modsec" ]
 then
-	echo -e "\nNo Results Found\n"
+	echo -e "\n***No Results Found***\n"
 else
-	echo "Results Found\n-------------\n"
+	echo -e "\n***Results Found***\n"
 	sleep 2s
 	echo $grep_modsec
 	echo ""
 fi
 sleep 1s
 
-echo -e "--------------------\ncPanel Access Log Results:"
+echo -e "--------------------\ncPanel Access Log Results:\n--------------------"
 grep_cPanelAccess=$( grep -a $ip /usr/local/cpanel/logs/access_log )
 if [ -z "$grep_cPanelAccess" ]
 then
-	echo "No Results Found"
+	echo -e "\n***No Results Found***\n"
 else
-	echo -e  "-------------\nResults Found\n"
+	echo -e "\n***Results Found***\n"
 	sleep 2s
 	echo $grep_cPanelAccess
 	echo ""
 fi
 sleep 1s
 
-echo -e "--------------------\ncPanel Error Log Results:"
+echo -e "--------------------\ncPanel Error Log Results:\n--------------------"
 grep_cPanelError=$( grep -a $ip /usr/local/cpanel/logs/error_log )
 if [ -z "$grep_cPanelError" ];
 then 
-	echo "No Results Found"
+	echo -e "\n***No Results Found***\n"
 else
-	echo -e  "-------------\nResults Found\n"
+	echo -e "\n***Results Found***\n"
 	sleep 2s
 	echo $grep_cPanelError
 	echo ""
 fi
 sleep 1s
 
-echo -e "--------------------\ncPanel Stats Log Results:"
+echo -e "--------------------\ncPanel Stats Log Results:\n--------------------"
 grep_cPanelStats=$( grep -a $ip /usr/local/cpanel/logs/stats_log )
 if [ -z "$grep_cPanelStats" ]
 then
-	echo "No Results Found"
+	echo -e "\n***No Results Found***\n"
 else
-	echo -e  "-------------\nResults Found\n"
+	echo -e "\n***Results Found***\n"
 	sleeps2
 	echo $grep_cPanelStats
 	echo ""
 fi
 sleep 1s
 
-echo -e "--------------------\ncPanel Check Service Log Results:"
+echo -e "--------------------\ncPanel Check Service Log Results:\n--------------------"
 grep_cPanelCheckService=$( grep -a $ip /var/log/chkservd.log )
 if [ -z "$grep_cPanelCheckService" ]
 then
-	echo "No Results Found"
+	echo -e "\n***No Results Found***\n"
 else
-	echo -e  "-------------\nResults Found\n"
+	echo -e "\n***Results Found***\n"
 	sleep 2s
 	echo $grep_cPanelCheckService
 	echo ""
 fi
 sleep 1s
 
-echo -e "--------------------\nExim Main Log Results:"
+echo -e "--------------------\nExim Main Log Results:\n--------------------"
 grep_EximMainLog=$( grep -a $ip /var/log/exim_mainlog )
 if [ -z "$grep_EximMainLog" ]
 then
-	echo "No Results Found"
+	echo -e "\n***No Results Found***\n"
 else
-	echo -e  "-------------\nResults Found\n"
+	echo -e "\n***Results Found***\n"
 	sleep 2s
 	echo $grep_EximMainLog
 	echo ""
 fi
 sleep 1s
 
-echo -e "--------------------\nExim Panic Log Results:"
+echo -e "--------------------\nExim Panic Log Results:\n--------------------"
 grep_EximPanicLog=$( grep -a $ip /var/log/exim_paniclog )
 if [ -z "$grep_EximPanicLog" ]
 then
-	echo "No Results Found"
+	echo -e "\n***No Results Found***\n"
 else
-	echo -e  "-------------\nResults Found\n"
+	echo -e "\n***Results Found***\n"
 	sleep 2s
 	echo $grep_EximPanicLog
 	echo ""
 fi
 sleep 1s
 
-echo -e "--------------------\nExim Reject Log Results:"
+echo -e "--------------------\nExim Reject Log Results:\n--------------------"
 grep_EximReject=$( grep -a $ip /var/log/exim_rejectlog )
 if [ -z "$grep_EximReject" ]
 then
-	echo "No Results Found"
+	echo -e "\n***No Results Found***\n"
 else
-	echo -e  "-------------\nResults Found\n"
+	echo -e "\n***Results Found***\n"
 	sleep 2s
 	echo $grep_EximReject
 	echo ""
 fi
 sleep 1s
 
-echo -e "--------------------\nlfd Log Results:"
+echo -e "-----------------\nlfd Log Results:\n-----------------"
 grep_lfd=$( grep -a $ip /var/log/lfd.log )
 if [ -z "$grep_lfd" ]
 then
-	echo "No Results Found"
+	echo -e "\n***No Results Found***\n"
 else
-	echo -e  "-------------\nResults Found\n"
+	echo -e "\n***Results Found***\n"
 	sleep 2s
 	echo $grep_lfd
 	echo ""
 fi
 sleep 1s
 
-echo "--------------------"
-echo "Thank you for using the IP search script!"
-echo "--------------------"
+echo -e "--------------------\nHope you found what you were looking for!\n--------------------"
 sleep 3s
 
 exit
